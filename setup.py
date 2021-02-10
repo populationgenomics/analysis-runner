64c7d66494a9e0fd9a82bb233d1bbc3a97f906dc
Michael Franklin <michael@illusional.net>
2021-02-10 11:17:47 +1100

    Add cli plus conda (#13)
    * Add conda package
    
    * Remove conda readme
    
    * Add shebang to cli.py
    
    * Fix shebang and "script" argument
    
    Co-authored-by: vladsaveliev <vladislav.sav@gmail.com>

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from os.path import join
+import setuptools
+
+PKG = 'analysis-runner'
+
+setuptools.setup(
+    name=PKG,
+    version='0.1.2',
+    description='Analysis runner to help make analysis results reproducible',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/populationgenomics/{PKG}',
+    license='MIT',
+    packages=['cli'],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[
+        join('cli', 'cli.py'),
+    ],
+    keywords='bioinformatics',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
