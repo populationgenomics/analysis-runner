3c2a1a05a46f83e5cb5b9d3131072bc97bdb57ac
MIchael Franklin <michael.franklin@populationgenomics.org.au>
2021-02-10 13:18:43 +1100

    Add logging to CLI

--- cli/cli.py
+++ cli/cli.py
@@ -4,21 +4,31 @@
 CLI for interfacing with deployed analysis runner.
 See README.md for more information.
 """
-
+import re
+import logging
 import subprocess
+
 import click
 import requests
 import google.auth
 import google.auth.transport.requests
+
+logging.basicConfig(level='INFO')
 
 BRANCH = 'add-cli'  # 'master'
 DEFAULT_SERVER_LOOKUP = (
     f'https://raw.githubusercontent.com/'
     f'populationgenomics/analysis-runner/{BRANCH}/cli/servermap.json'
 )
+# Make this None if you want to support all organisations
+SUPPORTED_ORGANISATIONS = {'populationgenomics'}
 
 
-@click.command()
+@click.command(
+    help='CLI for the analysis runner - a CPG service for running analysis from '
+    'some GitHub repository (at a specific commit). The parameters are used to form a '
+    'POST request, sent to a server based on the --dataset parameter you provide.'
+)
 @click.option(
     '--dataset',
     required=True,
@@ -27,6 +37,7 @@ DEFAULT_SERVER_LOOKUP = (
 )
 @click.option(
     '--output-dir',
+    '-o',
     required=True,
     help='The output directory of the run, MUST start with gs://',
 )
@@ -38,7 +49,7 @@ DEFAULT_SERVER_LOOKUP = (
     ' directory with `git remote get-url origin`',
 )
 @click.option(
-    '--commit-ref',
+    '--commit',
     '--hash',
     help='The hash of commit to run, default behaviour is to ' 'use the current commit',
 )
@@ -48,31 +59,42 @@ DEFAULT_SERVER_LOOKUP = (
     help='Description of job, otherwise defaults to: "$USER FROM LOCAL: $REPO@$COMMIT"',
 )
 @click.argument('script', nargs=-1)
-def main(dataset, output_dir, script, description, commit_ref=None, repository=None):
+def main(dataset, output_dir, script, description, commit=None, repository=None):
     """
     Main function that drives the CLI.
     The parameters are provided automatically by @click.
     """
     _repository = repository or _get_default_remote()
-    _commit_ref = commit_ref or _get_default_commit_ref(
+    _commit_ref = commit or _get_default_commit_ref(
         used_custom_repository=repository is not None
     )
     _url = _get_url_from_dataset(dataset)
     _token = _get_google_auth_token()
+    _script = script or ['main.py']
 
-    print(dataset, output_dir, script, _repository, _commit_ref, _token)
+    logging.info('Submitting %s@%s for dataset "%s"', _repository, _commit_ref, dataset)
 
-    requests.post(
+    response = requests.post(
         _url,
         json={
             'output': output_dir,
             'repo': _repository,
             'commit': _commit_ref,
-            'script': list(script),
+            'script': _script,
             'description': description,
         },
         headers={'Authorization': f'Bearer {_token}'},
     )
+    try:
+        response.raise_for_status()
+        logging.info('Request submitted successfully: %s', response.text)
+    except requests.HTTPError as e:
+        logging.critical(
+            'Request failed with status %s: %s\nFull response: %s',
+            response.status_code,
+            str(e),
+            response.text,
+        )
 
 
 def _get_google_auth_token() -> str:
@@ -105,7 +127,42 @@ def _get_output_of_command(command, description: str) -> str:
 
 def _get_default_remote() -> str:
     command = ['git', 'remote', 'get-url', 'origin']
-    return _get_output_of_command(command, 'get GIT repository')
+    full_remote = _get_output_of_command(command, 'get GIT repository')
+    return _get_repo_name_from_remote(full_remote)
+
+
+def _get_repo_name_from_remote(remote_name: str) -> str:
+    """
+    Turn the remote name received from 'git remote get-url origin' into the
+
+    >>> _get_repo_name_from_remote(\
+        'git@github.com:populationgenomics/analysis-runner.git'\
+    )
+    'analysis-runner'
+    >>> _get_repo_name_from_remote(\
+        'https://github.com/populationgenomics/analysis-runner.git'\
+    )
+    'analysis-runner'
+    """
+
+    organisation = None
+    repo = None
+    if remote_name.startswith('http'):
+        match = re.match(r'https:\/\/[A-z0-9\.]+?\/(.+?)\/(.+)$', remote_name)
+        organisation, repo = match.groups()
+    elif remote_name.startswith('git@'):
+        match = re.match(r'git@[A-z0-9\.]+?:(.+?)\/(.+)$', remote_name)
+        organisation, repo = match.groups()
+
+    if SUPPORTED_ORGANISATIONS and organisation not in SUPPORTED_ORGANISATIONS:
+        raise Exception(f'Unsupported GitHub organisation "{organisation}"')
+
+    if not repo:
+        raise Exception(f'Unsupported remote format: "{remote_name}"')
+    if repo.endswith('.git'):
+        repo = repo[:-4]
+
+    return repo
 
 
 def _get_default_commit_ref(used_custom_repository) -> str:
@@ -120,7 +177,6 @@ def _get_default_commit_ref(used_custom_repository) -> str:
 
 
 def _get_url_from_dataset(dataset: str) -> str:
-
     resource = requests.get(DEFAULT_SERVER_LOOKUP)
     d = resource.json()
 
@@ -135,3 +191,9 @@ if __name__ == '__main__':
     # Disable pylint because click decorates the function in a specific way
     # pylint: disable=no-value-for-parameter
     main()
+
+# if __name__ == '__main__':
+#     # Run tests
+#     import doctest
+#
+#     doctest.testmod()
