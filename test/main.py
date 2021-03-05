46a0e670796a8719aa0262ae2971351b81bd35cd
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-03-05 14:20:45 +1100

    Simpler test.

--- test/main.py
+++ test/main.py
@@ -1,19 +1,5 @@
 """A very simple batch that tests basic functionality."""
 
-import os
 import hail as hl
-import hailtop.batch as hb
 
 hl.init()
-
-backend = hb.ServiceBackend(
-    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
-)
-
-batch = hb.Batch(backend=backend, name='analysis-server-test')
-
-job = batch.new_job(name='test')
-job.image('australia-southeast1-docker.pkg.dev/analysis-runner/images/ubuntu:20.04')
-job.command('echo "hello world"')
-
-batch.run()
