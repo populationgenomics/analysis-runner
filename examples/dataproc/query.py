2f92fe871f9f246dd89a9552c3f5763024766934
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-03-20 18:31:24 +1100

    Add plot.

--- examples/dataproc/query.py
+++ examples/dataproc/query.py
@@ -1,7 +1,8 @@
 """Simple Hail query example."""
 
-import hail as hl
 import click
+import hail as hl
+from bokeh.io import export_png
 
 
 GNOMAD_HGDP_1KG_MT = (
@@ -12,16 +13,28 @@ GNOMAD_HGDP_1KG_MT = (
 
 @click.command()
 @click.option('--output', help='GCS output path', required=True)
-def query(output):
+@click.option('--rerun', help='Whether to overwrite cached files', default=False)
+def query(output, rerun):
     """Query script entry point."""
 
     hl.init(default_reference='GRCh38')
 
-    mt = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
-    mt = mt.head(100, n_cols=100)
+    sample_qc_path = f'{output}/sample_qc.mt'
+    if rerun or not hl.hadoop_exists(sample_qc_path):
+        mt = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
+        mt = mt.head(100, n_cols=100)
+        mt_qc = hl.sample_qc(mt)
+        mt_qc.write(sample_qc_path)
+    mt_qc = hl.read_matrix_table(sample_qc_path)
 
-    mt_qc = hl.sample_qc(mt)
-    mt_qc.write(f'{output}/sample_qc.mt')
+    plot_filename = f'{output}/call_rate_plot.png'
+    if rerun or not hl.hadoop_exists(plot_filename):
+        call_rate_plot = hl.plot.histogram(
+            mt_qc.sample_qc.call_rate, range=(0, 1), legend='Call Rate'
+        )
+        tmp_filename = 'plot.png'
+        export_png(call_rate_plot, tmp_filename)
+        hl.hadoop_copy(tmp_filename, plot_filename)
 
 
 if __name__ == '__main__':
