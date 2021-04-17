7469044a103ce84addc95d32d9b582f7d7618e78
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-17 13:06:15 +1000

    Bump version: 1.5.0 → 1.5.1 (#77)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PKG = 'analysis-runner'
 setuptools.setup(
     name=PKG,
     # This tag is automatically updated by bump2version
-    version='1.5.0',
+    version='1.5.1',
     description='Analysis runner to help make analysis results reproducible',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
