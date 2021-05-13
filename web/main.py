97a1648d25f206cf24a5c6c47127daae437ebaa6
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-05-13 18:24:53 +1000

    Use the correct IAP header for retrieving the email address

--- web/main.py
+++ web/main.py
@@ -1,35 +1,42 @@
 """Web server which proxies requests to per-dataset "web" buckets."""
 
+import logging
 import os
 from flask import Flask, abort, request
-from google.cloud import storage
+import google.cloud.storage
 from cpg_utils.cloud import is_google_group_member, email_from_id_token
 
 app = Flask(__name__)
 
-storage_client = storage.Client()
+storage_client = google.cloud.storage.Client()
+logger = logging.getLogger('gunicorn.error')
 
 
 @app.route('/<dataset>/<path:filename>')
 def handler(dataset=None, filename=None):
     """Main entry point for serving."""
     if not dataset or not filename:
+        logger.warning('Invalid request parameters')
         abort(400)
 
-    auth_header = request.headers.get('Authorization')
-    if not auth_header:
+    id_token = request.headers.get('x-goog-iap-jwt-assertion')
+    if not id_token:
+        logger.warning('Missing x-goog-iap-jwt-assertion header')
         abort(403)
 
     try:
-        email = email_from_id_token(auth_header)
+        email = email_from_id_token(id_token)
     except ValueError:
+        logger.warning('Failed to extract email from ID token')
         abort(403)
 
     group_name = f'{dataset}-access@populationgenomics.org.au'
     if not is_google_group_member(email, group_name):
+        logger.warning(f'{email} is not a member of {group_name}')
         abort(403)
 
     bucket_name = f'cpg-{dataset}-web'
+    logger.info(f'Fetching blob gs://{bucket_name}/{filename}')
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.get_blob(filename)
     if blob is None:
