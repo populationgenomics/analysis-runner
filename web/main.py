b7ec31eeaef0f8181a0a3da3266145fe2fa99c87
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-22 15:39:34 +1000

    Check access group cache for permissions in web server (#143)
    * Check access group cache for permissions in web server
    
    * Fix secretmanager init

--- web/main.py
+++ web/main.py
@@ -5,14 +5,19 @@ import mimetypes
 import os
 from flask import Flask, abort, request, Response
 import google.cloud.storage
-from cpg_utils.cloud import is_google_group_member, email_from_id_token
+import google.cloud.secretmanager
+import google.api_core.exceptions
+from cpg_utils.cloud import email_from_id_token
 
 BUCKET_SUFFIX = os.getenv('BUCKET_SUFFIX')
 assert BUCKET_SUFFIX
 
+PROJECT_ID = 'analysis-runner'
+
 app = Flask(__name__)
 
 storage_client = google.cloud.storage.Client()
+secret_manager = google.cloud.secretmanager.SecretManagerServiceClient()
 logger = logging.getLogger('gunicorn.error')
 
 
@@ -34,10 +39,20 @@ def handler(dataset=None, filename=None):
         logger.warning('Failed to extract email from ID token')
         abort(403)
 
-    group_name = f'{dataset}-access@populationgenomics.org.au'
-    if not is_google_group_member(email, group_name):
-        logger.warning(f'{email} is not a member of {group_name}')
-        abort(403)
+    try:
+        secret_name = f'{dataset}-access-members-cache'
+        secret_path = secret_manager.secret_path(PROJECT_ID, secret_name)
+        response = secret_manager.access_secret_version(
+            request={'name': f'{secret_path}/versions/latest'}
+        )
+
+        members = response.payload.data.decode('UTF-8').split(',')
+        if email not in members:
+            logger.warning(f'{email} is not a member of the {dataset} access group')
+            abort(403)
+    except google.api_core.exceptions.ClientError as e:
+        logger.warning(f'Error reading access group cache secret for "{dataset}": {e}')
+        abort(400)
 
     bucket_name = f'cpg-{dataset}-{BUCKET_SUFFIX}'
     logger.info(f'Fetching blob gs://{bucket_name}/{filename}')
