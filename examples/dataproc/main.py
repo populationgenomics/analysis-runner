659248a2c91d0f763b93c7db61ab7dcdd7a27f8a
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-28 16:08:32 +1000

    Remove unnecessary Hail init

--- examples/dataproc/main.py
+++ examples/dataproc/main.py
@@ -1,14 +1,11 @@
 """Demonstrates the use of the dataproc module."""
 
 import os
-import hail as hl
 import hailtop.batch as hb
 from analysis_runner import dataproc
 
 OUTPUT = os.getenv('OUTPUT')
 assert OUTPUT
-
-hl.init(default_reference='GRCh38')
 
 service_backend = hb.ServiceBackend(
     billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
