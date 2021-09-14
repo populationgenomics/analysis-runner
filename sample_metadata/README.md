477f1e99ea0cb67865fb479cf1e07948e98beebc
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-09-14 17:23:47 +1000

    Add skeleton for sample-metadata cloud-function

---
+++ sample_metadata/README.md
@@ -0,0 +1,15 @@
+# Sample metadata integration
+
+This Cloud Function listens to the Pub/Sub topic of analysis-runner submissions and updates the corresponding sample-metadata project.
+
+To deploy, run:
+
+```bash
+gcloud config set project analysis-runner
+
+gcloud functions deploy sample-metadata \
+     --runtime python37 \
+     --region australia-southeast1 \
+     --trigger-topic submissions \
+     --service-account sample-metadata@analysis-runner.iam.gserviceaccount.com
+```
---
+++ sample_metadata/main.py
@@ -0,0 +1,38 @@
+"""
+A Cloud Function to store analysis-runner submission metadata
+in the Sample-Metadata database.
+"""
+import requests
+
+
+def sample_metadata(data, unused_context):
+    """Puts analysis in sample-metadata"""
+    dataset = data.pop('dataset')
+    output_dir = data.pop('output')
+
+    data = {
+        # we don't know the sample_ids unfortunately :(
+        'sample_ids': [],
+        'type': 'custom',
+        'status': 'unknown',
+        'output': output_dir,
+        'author': data.pop('user'),
+        'meta': data,
+    }
+    token = get_identity_token()
+    requests.put(
+        f'https://sample-metadata.populationgenomics.org.au/api/v1/analysis/{dataset}',
+        data,
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+
+def get_identity_token() -> str:
+    """
+    Get identity token
+    Source: https://cloud.google.com/functions/docs/securing/function-identity#identity_tokens
+    """
+    url = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity'
+    r = requests.get(url=url, headers={'Metadata-Flavor': 'Google'})
+    r.raise_for_status()
+    return r.text
