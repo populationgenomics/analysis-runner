86d1a823053dbdb60f30b52802cd00f3668caf50
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-09-30 10:23:00 +1000

    remove SM

--- test/tmp_test.py
+++ test/tmp_test.py
@@ -2,12 +2,6 @@
 """Simple batch that tests basic functionality."""
 
 import hailtop.batch as hb
-from sample_metadata.api import AnalysisApi
-
-aapi = AnalysisApi()
-samples_without_analysis = aapi.get_all_sample_ids_without_analysis_type(
-    'gvcf', 'test_samples'
-)
 
 batch = hb.Batch(name='Test Batch Move Standard')
 
