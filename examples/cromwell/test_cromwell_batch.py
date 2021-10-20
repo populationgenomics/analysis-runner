ae46ed07ee50a60ad0c62008050581116d54112c
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-10-20 21:45:55 +1100

    Add json output option to cromwell status cli

---
+++ examples/cromwell/test_cromwell_batch.py
@@ -0,0 +1,53 @@
+import hailtop.batch as hb
+from analysis_runner.cromwell import (
+    run_cromwell_workflow,
+    watch_workflow_and_get_output,
+)
+from analysis_runner.git import (
+    prepare_git_job,
+    get_git_commit_ref_of_current_repository,
+)
+
+DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:dev'
+
+OUTPUT_SUFFIX = 'mfranklin/analysis-runner-test/out/'
+BUCKET = 'gs://cpg-fewgenomes-test/' + OUTPUT_SUFFIX
+
+sb = hb.ServiceBackend()
+b = hb.Batch(backend=sb, default_image=DRIVER_IMAGE)
+
+job = b.new_job('submit_cromwell')
+
+prepare_git_job(
+    job,
+    repo='analysis-runner',
+    commit=get_git_commit_ref_of_current_repository(),
+    is_test=True,
+)
+
+workflow_id_file = run_cromwell_workflow(
+    job=job,
+    dataset='fewgenomes',
+    access_level='test',
+    workflow='hello_all_in_one_file.wdl',
+    cwd="examples/cromwell",
+    libs=[],
+    output_suffix=OUTPUT_SUFFIX,
+    input_dict={"hello.inp": "test michael franklin"},
+)
+
+outputs_dict = watch_workflow_and_get_output(
+    b,
+    job_prefix='cromwell-workflow',
+    workflow_id_file=workflow_id_file,
+    outputs_to_collect={'out': None},
+    driver_image=DRIVER_IMAGE,
+)
+
+process_j = b.new_job('do-something-with-input')
+process_j.command(f'echo ${{$(cat {outputs_dict["out"]})}}')
+process_j.command(f'echo ${{$(cat {outputs_dict["out"]})}} > {process_j.out}')
+
+b.write_output(process_j.out, BUCKET)
+
+b.run()
