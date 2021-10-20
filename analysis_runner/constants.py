dc7d776d894c083c341a33d51d74e42ae9f51ae2
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-10-20 23:15:42 +1100

    Moves some constants to installable analysis_runner

---
+++ analysis_runner/constants.py
@@ -0,0 +1,5 @@
+# TODO: fix SERVER_ENDPOINT
+SERVER_ENDPOINT = 'https://server-test-a2pko7ameq-ts.a.run.app'
+ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
+CROMWELL_URL = 'https://cromwell.populationgenomics.org.au'
+CROMWELL_AUDIENCE = '717631777761-ec4u8pffntsekut9kef58hts126v7usl.apps.googleusercontent.com'
