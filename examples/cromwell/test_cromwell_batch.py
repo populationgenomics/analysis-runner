07120bb87b3f637fec7c12882e2dc826e909ba45
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-10-21 20:39:27 +1100

    Remove hard-coded DRIVER image for PR

--- examples/cromwell/test_cromwell_batch.py
+++ examples/cromwell/test_cromwell_batch.py
@@ -8,16 +8,14 @@ from analysis_runner.cromwell import (
     run_cromwell_workflow_from_repo_and_get_outputs,
 )
 
-# DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:dev'
-DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver@sha256:d06b6c2daa39699b17e10cfc3af0312214727d653a8cd7e0abd3ed58e7f060a0'
-
 OUTPUT_SUFFIX = 'mfranklin/analysis-runner-test/out/'
-BUCKET = 'gs://cpg-fewgenomes-test/' + OUTPUT_SUFFIX
+BUCKET = os.getenv('BUCKET')
+OUTPUT_PATH = os.path.join(f'gs://{BUCKET}', OUTPUT_SUFFIX)
 
 sb = hb.ServiceBackend(
-    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=BUCKET
 )
-b = hb.Batch(backend=sb, default_image=DRIVER_IMAGE)
+b = hb.Batch(backend=sb, default_image=os.getenv('DRIVER_IMAGE'))
 
 inputs = ['Hello, analysis-runner ;)', 'Hello, second output!']
 
@@ -28,11 +26,10 @@ workflow_outputs = run_cromwell_workflow_from_repo_and_get_outputs(
     access_level='test',
     workflow='hello_all_in_one_file.wdl',
     cwd='examples/cromwell',
-    libs=[],
+    libs=[], # hello_all_in_one_file is self-contained, so no dependencies
     output_suffix=OUTPUT_SUFFIX,
     input_dict={'hello.inps': inputs},
     outputs_to_collect={'hello.outs': len(inputs), 'hello.joined_out': None},
-    driver_image=DRIVER_IMAGE,
 )
 
 process_j = b.new_job('do-something-with-string-output')
