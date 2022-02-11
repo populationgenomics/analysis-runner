96c318ff7c4f423958f57675e4398532f2d0d7eb
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-02-11 11:49:05 +1100

    Parameritises hail batch workflow

--- examples/hailbatch/test.py
+++ examples/hailbatch/test.py
@@ -1,12 +1,22 @@
 import hailtop.batch as hb
+import click
 
-sb = hb.ServiceBackend()
-b = hb.Batch(backend=sb)
 
-j = b.new_job('first job')
-j.command(f'echo "Hello, there" > {j.outfile}')
+@click.command()
+@click.option('--name-to-print')
+def main(name_to_print):
 
-j2 = b.new_job('second job')
-j2.command(f'cat {j.outfile}')
+    sb = hb.ServiceBackend()
+    b = hb.Batch(backend=sb)
 
-b.run(wait=False)
+    j = b.new_job('first job')
+    j.command(f'echo "Hello, {name_to_print}" > {j.outfile}')
+
+    j2 = b.new_job('second job')
+    j2.command(f'cat {j.outfile}')
+
+    b.run(wait=False)
+
+
+if __name__ == "__main__":
+    main()
