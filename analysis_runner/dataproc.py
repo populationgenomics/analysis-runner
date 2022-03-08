e6e2778ef9a57a70f22d841992c53d81a991cb21
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-09 08:38:48 +1100

    Switch to standard Hail package in pypi

--- analysis_runner/dataproc.py
+++ analysis_runner/dataproc.py
@@ -18,7 +18,7 @@ from analysis_runner.git import (
 
 
 DATAPROC_IMAGE = (
-    'australia-southeast1-docker.pkg.dev/analysis-runner/images/dataproc:hail-0.2.85'
+    'australia-southeast1-docker.pkg.dev/analysis-runner/images/dataproc:hail-0.2.89'
 )
 GCLOUD_PROJECT = f'gcloud config set project {os.getenv("DATASET_GCP_PROJECT")}'
 DATAPROC_REGION = 'gcloud config set dataproc/region australia-southeast1'
