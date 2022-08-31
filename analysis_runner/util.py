3abc9bfa90c8db85814ccb5cdddec008950a3f07
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-08-31 14:16:37 +1000

    Revert "Revert "Fix fetching of ID tokens when using a service account (#487)" (#490)"
    This reverts commit 43cb4ce72c4a8601846a453a4b8c92821926fe32.

--- analysis_runner/util.py
+++ analysis_runner/util.py
@@ -103,19 +103,14 @@ def confirm_choice(choice: str):
         print('Unrecognised option, please try again.')
 
 
-def get_google_identity_token() -> str:
+def get_google_identity_token(audience) -> str:
     """
-    Get google identity token, equivalent of calling:
-        ['gcloud', 'auth', 'print-identity-token']
+    Returns a Google identity token for the given audience.
     """
-    import google.auth
-    import google.auth.transport.requests
+    from google.auth.transport.requests import Request
+    from google.oauth2 import id_token
 
-    # https://stackoverflow.com/a/55804230
-    creds, _ = google.auth.default()
-    auth_req = google.auth.transport.requests.Request()
-    creds.refresh(auth_req)
-    return creds.id_token
+    return id_token.fetch_id_token(Request(), audience=audience)
 
 
 def _perform_version_check():
