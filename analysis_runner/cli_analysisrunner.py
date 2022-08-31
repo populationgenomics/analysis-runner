5091f9773c8fa6aa66a8eb273e43ac386d7562e0
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-08-31 22:56:54 +1000

    Bump version: 2.32.6 → 2.32.7: Use get_google_identity_token from cpg_utils.cloud (#491)
    * Revert "Revert "Fix fetching of ID tokens when using a service account (#487)" (#490)"
    
    This reverts commit 43cb4ce72c4a8601846a453a4b8c92821926fe32.
    
    * Undo version bump
    
    * Use get_google_identity_token from cpg_utils.cloud
    
    * Bump version: 2.32.6 → 2.32.7

--- analysis_runner/cli_analysisrunner.py
+++ analysis_runner/cli_analysisrunner.py
@@ -9,6 +9,7 @@ from typing import List
 
 import requests
 from cpg_utils.config import read_configs
+from cpg_utils.cloud import get_google_identity_token
 from analysis_runner.constants import get_server_endpoint
 from analysis_runner.git import (
     get_git_default_remote,
@@ -22,7 +23,6 @@ from analysis_runner.util import (
     _perform_version_check,
     confirm_choice,
     logger,
-    get_google_identity_token,
 )
 
 
@@ -191,12 +191,13 @@ def run_analysis_runner(  # pylint: disable=too-many-arguments
     if config:
         _config = read_configs(config)
 
-    _token = get_google_identity_token()
+    server_endpoint = get_server_endpoint(is_test=use_test_server)
+    _token = get_google_identity_token(server_endpoint)
 
     logger.info(f'Submitting {_repository}@{_commit_ref} for dataset "{dataset}"')
 
     response = requests.post(
-        get_server_endpoint(is_test=use_test_server),
+        server_endpoint,
         json={
             'dataset': dataset,
             'output': output_dir,
