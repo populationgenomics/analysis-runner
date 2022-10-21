f1bd7973a6ff92441771f461c36d28b9dc4b6df5
Matt Welland <mattwellie@gmail.com>
2022-10-21 14:37:22 +1100

    script for copying clinvar to CPG reference (#533)
    * script for copying clinvar to CPG reference

---
+++ scripts/copy_clinvar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# this should be run with high-core low memory resources
+# e.g.
+# analysis-runner analysis-runner
+#  --dataset reference \
+#  --description "Copy Clinvar Broad -> CPG" \
+#  -o "clinvar_copy" \
+#  --access-level standard \
+#  --cpu 16
+#  --memory lowmem
+#  scripts/copy_clinvar.sh
+#  2022-09-17
+
+set -ex
+
+DATE=${1}
+CPG_ANNO=${2:-"gs://cpg-reference/seqr/"}
+
+gcloud storage cp -r "gs://seqr-reference-data/GRCh38/clinvar/clinvar.GRCh38.${DATE}.ht" "${CPG_ANNO}"
