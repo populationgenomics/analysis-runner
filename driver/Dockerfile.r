615b2c4b671161acb61c3018b9f1e22a7ebb9d2a
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-10-24 12:43:54 +1100

    Add C compiler to R Docker image

--- driver/Dockerfile.r
+++ driver/Dockerfile.r
@@ -4,7 +4,12 @@ FROM australia-southeast1-docker.pkg.dev/analysis-runner/images/driver-base:1.3
 ENV MAMBA_ROOT_PREFIX /root/micromamba
 ENV PATH $MAMBA_ROOT_PREFIX/bin:$PATH
 
-RUN wget -qO- https://api.anaconda.org/download/conda-forge/micromamba/0.8.2/linux-64/micromamba-0.8.2-he9b6cbd_0.tar.bz2 | tar -xvj -C /usr/local bin/micromamba && \
+RUN apt update && \
+    # Some R packages require a C compiler during installation.
+    apt install -y build-essential && \
+    rm -r /var/lib/apt/lists/* && \
+    rm -r /var/cache/apt/* && \
+    wget -qO- https://api.anaconda.org/download/conda-forge/micromamba/0.8.2/linux-64/micromamba-0.8.2-he9b6cbd_0.tar.bz2 | tar -xvj -C /usr/local bin/micromamba && \
     mkdir $MAMBA_ROOT_PREFIX && \
     micromamba install -y --prefix $MAMBA_ROOT_PREFIX \
         -c cpg -c bioconda -c conda-forge \
