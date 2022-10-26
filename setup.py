795861f7f0d13a6cd7510a8703f1970e4025e027
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-10-26 16:44:05 +1100

    Require current cpg-utils version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     zip_safe=False,
     install_requires=[
         'cloudpathlib[all]',
-        'cpg-utils>=4.8.5',
+        'cpg-utils>=4.9.4',
         'click',
         'airtable-python-wrapper',
         'hail',
