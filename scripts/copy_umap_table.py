45ec5521ce8cd2db3c0e0eb4afe82a15749cd0b3
MattWellie <mattwellie@gmail.com>
2022-12-28 19:38:09 +1100

    scrap it

--- scripts/copy_umap_table.py
+++ scripts/copy_umap_table.py
@@ -8,12 +8,7 @@ import sys
 
 import hail as hl
 
-from cpg_utils.hail_batch import (
-    copy_common_env,
-    get_config,
-    init_batch,
-    output_path,
-)
+from cpg_utils.hail_batch import init_batch, output_path
 from cpg_workflows.batch import get_batch
 
 
@@ -26,50 +21,14 @@ logging.basicConfig(
 )
 
 
-def table_things(block_zipped: str, output: str):
+if __name__ == '__main__':
+    init_batch()
     """
     take the re-bgzipped bed file, ingest in hail
     update a datatype, and write out to cloud
-
-    :param block_zipped:
-    :param output:
     """
-    init_batch()
-    ht = hl.import_bed(block_zipped)
+    ht = hl.import_bed('gs://cpg-tob-wgs-test/matt_umap/umap_bedgraph.bgz')
 
     # swap the type to a float (defaults to string)
     ht = ht.transmute(target=hl.float64(ht.target))
-    ht.write(output)
-
-
-if __name__ == '__main__':
-    init_batch()
-
-    # # add a script job
-    # bash_job = get_batch().new_bash_job(name='WGet UMap')
-    # file = 'https://bismap.hoffmanlab.org/raw/hg38/k50.umap.bedgraph.gz'
-    # bash_job.image('australia-southeast1-docker.pkg.dev/cpg-common/images/samtools:1.16.1')
-    # bash_job.command(
-    #     (
-    #         f'wget {file} &&'
-    #         f'gunzip -c k50.umap.bedgraph.gz | bgzip > {bash_job.output}'
-    #     )
-    # )
-    # out_path_bgz = output_path('umap_bedgraph.bgz')
-    # get_batch().write_output(bash_job.output, out_path_bgz,)
-
-    python_job = get_batch().new_bash_job(name='ingest and write table')
-    copy_common_env(python_job)
-
-    python_job.image(get_config()['workflow']['driver_image'])
-    out_path = output_path('umap_table.ht')
-    print(type(table_things))
-    print(type(out_path))
-    _pyresult = python_job.call(
-        table_things,
-        block_zipped='gs://cpg-tob-wgs-test/matt_umap/umap_bedgraph.bgz',
-        output=out_path,
-    )
-    # python_job.depends_on(bash_job)
-
-    get_batch().run(wait=False)
+    ht.write(output_path('umap_table.ht'))
