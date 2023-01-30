6abff6ddc0e6fb7802a91288ca4228741294c04d
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-01-30 22:45:47 +1100

    Add output_suffix to hello example

--- examples/batch/hello.py
+++ examples/batch/hello.py
@@ -13,12 +13,13 @@ Test Hail Batch Workflow
 from shlex import quote
 import hailtop.batch as hb
 import click
-from cpg_utils.hail_batch import get_config, remote_tmpdir
+from cpg_utils.hail_batch import get_config, remote_tmpdir, output_path
 
 
 @click.command()
 @click.option('--name-to-print')
-def main(name_to_print):
+@click.option('--output-suffix', required=False)
+def main(name_to_print, output_suffix):
     """Runs test hail batch workflow"""
     config = get_config()
 
@@ -40,6 +41,8 @@ def main(name_to_print):
     # for the second job, using an f-string with the resource file
     # will tell batch to run j2 AFTER j1
     j2.command(f'cat {stdout_of_j}')
+
+    b.write_output(stdout_of_j, output_path(output_suffix))
 
     # use wait=False, otherwise this line will hang while the sub-batch runs
     # bad for running hail batch within a hail batch, as preemption
