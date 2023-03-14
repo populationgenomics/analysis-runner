eecb4018255c6b5db429b1b314972b5fd0fe64ab
EddieLF <34049565+EddieLF@users.noreply.github.com>
2023-03-14 16:18:46 +1100

    Update to copy crams into release bucket script (#593)
    * testing copycrams AR call
    
    * copy from test to test
    
    * copy from main to release
    
    * lint fix
    
    * add check if cram exists before copy

--- scripts/copy_sample_cram_to_release.py
+++ scripts/copy_sample_cram_to_release.py
@@ -9,37 +9,65 @@ each sample listed into the project's release bucket.
 import logging
 import sys
 import subprocess
+import time
 import click
 
 # pylint: disable=E0401,E0611
+from cpg_utils.config import get_config
 from sample_metadata.apis import AnalysisApi
 from sample_metadata.models import AnalysisType
 
 
-def copy_to_release(project: str, path: str):
+def check_paths_exist(paths: list[str]):
     """
-    Copy a single file from a main bucket path to the equivalent release bucket
+    Checks a list of gs:// paths to see if they point to an existing blob
+    Logs the invalid paths if any are found
     """
-    release_path = path.replace(
-        f'cpg-{project}-main',
-        f'cpg-{project}-release',
-    )
+    invalid_paths = False
+    for path in paths:
+        # gsutil ls <path> returns '<path>\n' if path exists
+        result = subprocess.run(
+            ['gsutil', 'ls', path], check=True, capture_output=True, text=True
+        ).stdout.strip('\n')
+        if result == path:
+            continue
+        # If path does not exist, log the path and set invalid_paths to True
+        logging.info(f'Invalid path: {path}')
+        invalid_paths = True
 
-    subprocess.run(['gsutil', 'cp', '-u', project, path, release_path], check=True)
-    logging.info(f'Copied {release_path}')
+    if invalid_paths:
+        return False
+    return True
+
+
+def copy_to_release(project: str, paths: list[str]):
+    """
+    Copy many files from main bucket paths to the release bucket with todays date as directory
+    """
+    today = time.strftime('%Y-%m-%d')
+    release_path = f'gs://cpg-{project}-release/{today}/'
+
+    subprocess.run(
+        ['gcloud', 'storage', '--billing-project', project, 'cp', *paths, release_path],
+        check=True,
+    )
+    logging.info(f'Copied {paths} into {release_path}')
 
 
 @click.command()
-@click.option('--project', '-p', help='Metamist name of the project', required=True)
+@click.option('--project', '-p', help='Metamist name of the project', default='')
 @click.argument('samples', nargs=-1)
 def main(project: str, samples):
     """
 
     Parameters
     ----------
-    project :   a metamist project name
+    project :   a metamist project name, optional as it can be pulled from the AR config
     samples :   a list of sample ids to copy to the release bucket
     """
+    if not project:
+        config = get_config()
+        project = config['workflow']['dataset']
 
     sample_ids = list(samples)
 
@@ -48,10 +76,15 @@ def main(project: str, samples):
         AnalysisType('cram'), project, request_body=sample_ids
     )
 
-    # Copy files to test
+    # Get all paths of files to be copied to release
+    cram_paths = []
     for cram in latest_crams:
-        copy_to_release(project, cram['output'])
-        copy_to_release(project, cram['output'] + '.crai')
+        cram_paths.append(cram['output'])
+        cram_paths.append(cram['output'] + '.crai')
+
+    # Check if all paths are valid and execute the copy commands if they are
+    if check_paths_exist(cram_paths):
+        copy_to_release(project, cram_paths)
 
 
 if __name__ == '__main__':
