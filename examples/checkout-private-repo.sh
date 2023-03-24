91be33d3cf9a95536ddf5e14e38477ba514de0d5
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-03-24 12:35:31 +1100

    Improve logging

--- examples/checkout-private-repo.sh
+++ examples/checkout-private-repo.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -x
+
 # get secret names from config if they exist
 secret_name=$(python -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_name', ''))")
 secret_project=$(python -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_project', ''))")
 
 if [ ! -z "$secret_name" ] && [ ! -z "$secret_project" ]; then
     # configure git credentials store if credentials are set
+    echo 'Configuring private repo'
     gcloud --project $secret_project secrets versions access --secret $secret_name latest > ~/.git-credentials
     git config --global credential.helper "store"
 fi
