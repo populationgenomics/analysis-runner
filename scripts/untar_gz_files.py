0e47b0d12ec4516466882db40d2844a5e90bcd04
Matt Welland <mattwellie@gmail.com>
2023-03-27 08:37:54 +1030

    shuffle original script with a wrapper (#604)
    * shuffle original script with a wrapper
    
    * copy_common_env?
    
    * sys.exit
    
    * black

--- scripts/untar_gz_files.py
+++ scripts/untar_gz_files.py
@@ -2,143 +2,85 @@
 
 
 """
-Given a gcp directory, extract all .tar.gz files in the directory
-into output_dir defined in the call to analysis runner
+Given a single TAG.GZ, extract and upload
 """
 
 import logging
 import os
-import re
 import subprocess
 import sys
 import click
 
 # pylint: disable=E0401,E0611
-from cpg_utils.config import get_config
 from google.cloud import storage
-
-RMATCH_STR = r'gs://(?P<bucket>[\w-]+)/(?P<suffix>.+)/'
-path_pattern = re.compile(RMATCH_STR)
 
 client = storage.Client()
 
 
-def get_path_components_from_path(path):
+@click.command()
+@click.option('--bucket', help='', required=True)
+@click.option('--subdir', help='', required=True)
+@click.option('--blob_name', help='', required=True)
+@click.option('--outdir', help='', required=True)
+def main(bucket: str, subdir: str, blob_name: str, outdir: str):
     """
-    Returns the bucket_name and subdir for GS only paths
-    Uses regex to match the bucket name and the subdirectory.
+    runs an extraction and upload for a single file
+
+    TODO - could make this even easier by having the wrapper
+    TODO - copy the single tarball into the job image
+    TODO - leaving extraction and upload as the only tasks
+
+    :param bucket: str, name of the bucket to connect to
+    :param subdir: str, name of the path within the bucket
+    :param blob_name: str, name of the tarball
+    :param outdir: str, where to write to
     """
 
-    path_components = (path_pattern.match(path)).groups()
+    input_bucket = client.get_bucket(bucket)
 
-    bucket_name = path_components[0]
-    subdir = path_components[1]
+    # Make the extraction directory on the disk
+    if not os.path.exists(f'./{subdir}'):
+        os.makedirs(f'./{subdir}')
+        os.makedirs(f'./{subdir}/extracted')
 
-    return bucket_name, subdir
+    # Download and extract the tarball
+    input_bucket.get_blob(blob_name).download_to_filename(blob_name)
+    logging.info(f'Untaring {blob_name}')
+    subprocess.run(
+        ['tar', '-xzf', f'{blob_name}', '-C', f'./{subdir}/extracted'],
+        check=True,
+    )
+    logging.info(f'Untared {blob_name}')
 
+    extracted_from_tarball = os.listdir(f'./{subdir}/extracted')
 
-def get_tarballs_from_path(bucket_name: str, subdir: str):
-    """
-    Checks a gs://bucket/subdir/ path for .tar.gz files
-    Returns a list of .tar.gz blob paths found in the subdirectory
-    """
+    # Check if the tarball compressed a single directory, if yes then get files inside
+    if os.path.isdir(f'./{subdir}/extracted/{extracted_from_tarball[0]}'):
+        is_directory = True
+        folder = extracted_from_tarball[0]
+        extracted_files = os.listdir(f'./{subdir}/extracted/{folder}')
+    else:
+        is_directory = False
+        extracted_files = os.listdir(f'./{subdir}/extracted')
+    logging.info(f'Extracted {extracted_files}')
 
-    blob_names = []
-    for blob in client.list_blobs(bucket_name, prefix=(subdir + '/'), delimiter='/'):
-        if not blob.name.endswith('.tar.gz'):
-            continue
-        blob_names.append(blob.name)
+    # Iterate through extracted files, upload them to bucket, then delete them
+    for file in extracted_files:
+        output_blob = input_bucket.blob(os.path.join(subdir, outdir, file))
 
-    logging.info(f'{len(blob_names)} .tar.gz files found in {subdir} of {bucket_name}')
-
-    return blob_names
-
-
-def untar_gz_files(
-    bucket_name: str,
-    subdir: str,
-    blob_names: list[str],
-    destination: str,
-):
-    """
-    Downloads the .tar.gz files in blob_names to the disk
-    Extracts the files from the tarball into a folder called extracted
-    Uploads and then deletes each extracted file, one by one.
-    Deletes the original downloaded tarball after all its files are uploaded.
-
-    The uploaded files end up in a directory denoted by the destination
-    appended to the original gs:// search path.
-    """
-    input_bucket = client.get_bucket(bucket_name)
-
-    for blob_name in blob_names:
-        # Make the extraction directory on the disk
-        if not os.path.exists(f'./{subdir}'):
-            os.makedirs(f'./{subdir}')
-            os.makedirs(f'./{subdir}/extracted')
-
-        # Download and extract the tarball
-        input_bucket.get_blob(blob_name).download_to_filename(blob_name)
-        logging.info(f'Untaring {blob_name}')
-        subprocess.run(
-            ['tar', '-xzf', f'{blob_name}', '-C', f'./{subdir}/extracted'],
-            check=True,
-        )
-        logging.info(f'Untared {blob_name}')
-
-        extracted_from_tarball = os.listdir(f'./{subdir}/extracted')
-
-        # Check if the tarball compressed a single directory, if yes then get files inside
-        if os.path.isdir(f'./{subdir}/extracted/{extracted_from_tarball[0]}'):
-            is_directory = True
-            folder = extracted_from_tarball[0]
-            extracted_files = os.listdir(f'./{subdir}/extracted/{folder}')
+        if is_directory:
+            filepath = f'./{subdir}/extracted/{folder}/{file}'
         else:
-            is_directory = False
-            extracted_files = os.listdir(f'./{subdir}/extracted')
-        logging.info(f'Extracted {extracted_files}')
+            filepath = f'./{subdir}/extracted/{file}'
 
-        # Iterate through extracted files, upload them to bucket, then delete them
-        for file in extracted_files:
-            output_blob = input_bucket.blob(os.path.join(subdir, destination, file))
+        output_blob.upload_from_filename(filepath)
+        logging.info(f'Uploaded {file} to gs://{bucket}/{subdir}/{outdir}/')
 
-            if is_directory:
-                filepath = f'./{subdir}/extracted/{folder}/{file}'
-            else:
-                filepath = f'./{subdir}/extracted/{file}'
-
-            output_blob.upload_from_filename(filepath)
-            logging.info(
-                f'Uploaded {file} to gs://{bucket_name}/{subdir}/{destination}/'
-            )
-
-            # Delete file after upload
-            subprocess.run(['rm', f'{filepath}'], check=True)
-            logging.info(f'Deleted {file} from disk')
-
-        # Delete tarball after all extracted files uploaded & deleted
-        subprocess.run(['rm', f'{blob_name}'], check=True)
-        logging.info(f'Deleted tarball {blob_name}')
+        # Delete file after upload
+        subprocess.run(['rm', f'{filepath}'], check=True)
+        logging.info(f'Deleted {file} from disk')
 
     logging.info('All tarballs extracted and uploaded. Finishing...')
-
-
-@click.command()
-@click.option('--search-path', '-p', help='GCP bucket/directory to search', default='')
-def main(search_path: str):
-    """
-    Parameters
-    ----------
-    search_path :   The GCP directory containing the .tar.gz files
-    """
-    config = get_config()
-    output_dir = config['workflow']['output_prefix']
-
-    bucket_name, subdir = get_path_components_from_path(search_path)
-
-    blobs = get_tarballs_from_path(bucket_name, subdir)
-
-    untar_gz_files(bucket_name, subdir, blobs, output_dir)
 
 
 if __name__ == '__main__':
