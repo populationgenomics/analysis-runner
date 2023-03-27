4867045d74393197b80f915a746582ee3c124923
Michael Franklin <michael@illusional.net>
2023-03-27 14:20:08 +1100

    Use Python3

--- analysis_runner/git.py
+++ analysis_runner/git.py
@@ -169,8 +169,8 @@ def prepare_git_job(
         job.command(
             """
 # get secret names from config if they exist
-secret_name=$(python -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_name', ''))")
-secret_project=$(python -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_project', ''))")
+secret_name=$(python3 -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_name', ''))")
+secret_project=$(python3 -c "from cpg_utils.config import get_config; print(get_config(print_config=False).get('infrastructure', {}).get('git_credentials_secret_project', ''))")
 
 if [ ! -z "$secret_name" ] && [ ! -z "$secret_project" ]; then
 # configure git credentials store if credentials are set
