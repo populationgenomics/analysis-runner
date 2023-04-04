194952159d792b2c0816a3d569ecebf667492e3f
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-04-04 16:41:27 +1000

    Bump version: 2.40.4 → 2.40.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_file(filename: str) -> str:
 setuptools.setup(
     name=PKG,
     # This tag is automatically updated by bump2version
-    version='2.40.4',
+    version='2.40.5',
     description='Analysis runner to help make analysis results reproducible',
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
