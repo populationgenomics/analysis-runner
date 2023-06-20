c7b9cb2c65c39114ded82efb0c94b1110c7a052c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-06-21 09:35:00 +1000

    Create copy_cram_for_str.sh

---
+++ scripts/copy_cram_for_str.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gsutil cp "gs://cpg-tob-wgs-main/cram/nagim/CPG3772.cram" "gs://cpg-tob-wgs-test/cram/nagim/CPG3772.cram"
+gsutil cp "gs://cpg-tob-wgs-main/cram/nagim/CPG3772.cram.crai" "gs://cpg-tob-wgs-test/cram/nagim/CPG3772.cram.crai"
