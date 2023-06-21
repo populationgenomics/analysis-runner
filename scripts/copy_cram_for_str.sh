b8a1c5778fa030ab93e73a065cfc34b2378314eb
Matt Welland <mattwellie@gmail.com>
2023-06-21 16:21:03 +1000

    Update scripts/copy_cram_for_str.sh

--- scripts/copy_cram_for_str.sh
+++ scripts/copy_cram_for_str.sh
@@ -7,3 +7,4 @@ gsutil cp "gs://cpg-tob-wgs-main/cram/nagim/CPG3772.cram.crai" "gs://cpg-tob-wgs
 # check optical duplicate issue
 gsutil cp "gs://cpg-tob-wgs-main/cram/nagim/CPG67314.cram" "gs://cpg-tob-wgs-test/cram/nagim/CPG67314.cram"
 gsutil cp "gs://cpg-tob-wgs-main/cram/nagim/CPG67314.cram.crai" "gs://cpg-tob-wgs-test/cram/nagim/CPG67314.cram.crai"
+
