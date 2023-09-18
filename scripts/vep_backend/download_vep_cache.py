fd0a8ad07f9dba56fcec67e42ac9eb1b8669af60
MattWellie <mattwellie@gmail.com>
2023-09-18 15:58:34 +1000

    downloads the VEP cache

---
+++ scripts/vep_backend/download_vep_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+download the VEP cache file and write it into GCP
+"""
+
+
+from cpg_workflows.batch import get_batch, dataset_path
+
+
+# nothin' to it but to do it - copy that file
+b = get_batch(name='download VEP 110 cache')
+j = b.new_job(name='curl to local')
+j.command(f'curl https://ftp.ensembl.org/pub/release-110/variation/vep/homo_sapiens_vep_110_GRCh38.tar.gz > {j.ofile}')
+b.write_output(j.ofile, dataset_path('VEP_110_cache'))
+b.run(wait=False)
