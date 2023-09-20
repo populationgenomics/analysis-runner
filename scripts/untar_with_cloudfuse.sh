27cfa5928257fa994e8849af861e94a93642d564
EddieLF <eddieformaini@gmail.com>
2023-09-20 11:51:35 +1000

    Bash script to install GCS Fuse and untar a file

--- scripts/untar_with_cloudfuse.sh
+++ scripts/untar_with_cloudfuse.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -9,6 +11,6 @@ gcsfuse -v
 
 mkdir $HOME/mounted
 
-gcsfuse --only-dir "2023-09-20" cpg-schr-neuro-test-upload $HOME/mounted
+gcsfuse --only-dir 2023-09-20 cpg-schr-neuro-test-upload $HOME/mounted
 
-tar -xvf "$HOME/mounted/2023-09-20/test.tar" -C "$HOME/mounted/2023-09-20/extracted"
+tar -xvf $HOME/mounted/2023-09-20/test.tar -C $HOME/mounted/2023-09-20/extracted
