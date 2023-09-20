17cac820a754d605c5465e312cd5b8eeaf25f4cd
EddieLF <eddieformaini@gmail.com>
2023-09-20 12:25:21 +1000

    Also install sudo

--- scripts/untar_with_cloudfuse.sh
+++ scripts/untar_with_cloudfuse.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+apt-get update
+apt-get install sudo
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-apt-get update
-apt-get install -y gcsfuse
+sudo apt-get update
+sudo apt-get install -y gcsfuse
 
 gcsfuse -v
 
