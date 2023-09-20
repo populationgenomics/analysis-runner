f3abc1bf6150fd5d5602110d8f1e13f15cf09321
EddieLF <eddieformaini@gmail.com>
2023-09-20 12:01:15 +1000

    Remove sudo commands

--- scripts/untar_with_cloudfuse.sh
+++ scripts/untar_with_cloudfuse.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-sudo apt-get update
-sudo apt-get install gcsfuse
+apt-get update
+apt-get install gcsfuse
 
 gcsfuse -v
 
