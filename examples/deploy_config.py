2c1d2ec6c77f7523831943f74f55ec22fbbafb2c
Michael Franklin <illusional@users.noreply.github.com>
2023-10-04 12:15:47 +1100

    Print hail version

--- examples/deploy_config.py
+++ examples/deploy_config.py
@@ -1,7 +1,8 @@
+from hail import version
 from hailtop.config import get_deploy_config
 
 config = get_deploy_config()
 
-
+print(version())
 print(config.get_config())
 print(config.base_url('batch'))
