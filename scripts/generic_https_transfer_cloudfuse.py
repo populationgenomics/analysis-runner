17dadf4c215a2545f181365d6b81eb00f605ecdc
EddieLF <eddieformaini@gmail.com>
2024-02-29 08:23:24 +1100

    Use cloudfuse for https transfer and tar xvf scripts

---
+++ scripts/generic_https_transfer_cloudfuse.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+"""
+Transfer datasets from presigned URLs to a dataset's GCP main-upload bucket.
+"""
+
+import os
+from shlex import quote
+
+import click
+from cloudpathlib import AnyPath
+from cpg_utils.config import get_config
+from cpg_utils.hail_batch import (
+    authenticate_cloud_credentials_in_job,
+    dataset_path,
+    get_batch,
+)
+
+
+@click.command('Transfer_datasets from signed URLs')
+@click.option(
+    '--filenames',
+    is_flag=True,
+    default=False,
+    help='Use filenames defined before each url',
+)
+@click.option('--presigned-url-file-path')
+def main(presigned_url_file_path: str, filenames: bool):
+    """
+    Given a list of presigned URLs, download the files and upload them to GCS.
+    If each signed url is prefixed by a filename and a space, use the --filenames flag
+    GCP suffix in target GCP bucket is defined using analysis-runner's --output
+    """
+
+    env_config = get_config()
+    billing_project = env_config['hail']['billing_project']
+    dataset = env_config['workflow']['dataset']
+    output_prefix = env_config['workflow']['output_prefix']
+    assert all({billing_project, dataset, output_prefix})
+    names = None
+    with AnyPath(presigned_url_file_path).open() as file:
+        if filenames:
+            names = [
+                line.strip().split(' ')[0] for line in file.readlines() if line.strip()
+            ]
+            #  reset readlines() to start of file
+            print(f'seeking to line {file.seek(0)}')
+            presigned_urls = [
+                line.strip().split(' ')[1] for line in file.readlines() if line.strip()
+            ]
+        else:
+            presigned_urls = [line.strip() for line in file.readlines() if line.strip()]
+
+    incorrect_urls = [url for url in presigned_urls if not url.startswith('https://')]
+    if incorrect_urls:
+        raise ValueError(f'Incorrect URLs: {incorrect_urls}')
+
+    batch = get_batch(name=f'transfer {dataset}', default_image='gcsfuse')
+    output_path = dataset_path(output_prefix, 'upload')
+    bucket_name = output_path.removeprefix('gs://').split('/')[0]
+
+    # may as well batch them to reduce the number of VMs
+    for idx, url in enumerate(presigned_urls):
+        if names:
+            filename = names[idx]
+        else:
+            filename = os.path.basename(url).split('?')[0]
+            
+        output_file_path = output_path.removeprefix(f'gs://') + f'/{filename}'
+        j = batch.new_job(f'URL {idx} ({filename})')
+        quoted_url = quote(url)
+        authenticate_cloud_credentials_in_job(job=j)
+        # catch errors during the cURL
+        
+        j.command('set -euxo pipefail')
+        j.cloudfuse(bucket=bucket_name, mount_point=f'/{bucket_name}').command(
+            f'curl -L {quoted_url} -o /{output_file_path}'
+        )
+
+    batch.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
---
+++ scripts/unzip_wrapper_cloudfuse.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+"""
+wrapper script for the un-tar script
+wrapped to modulate the batch job storage
+"""
+
+import logging
+import os
+import re
+import subprocess
+import sys
+
+import click
+from google.cloud import storage
+
+from cpg_utils import to_path
+from cpg_utils.config import get_config
+from cpg_utils.git import prepare_git_job
+from cpg_utils.hail_batch import (
+    authenticate_cloud_credentials_in_job,
+    copy_common_env,
+    get_batch,
+)
+
+CLIENT = storage.Client()
+RMATCH_STR = r'gs://(?P<bucket>[\w-]+)/(?P<suffix>.+)/'
+PATH_PATTERN = re.compile(RMATCH_STR)
+GB = 1024 * 1024 * 1024  # dollars
+UNZIP_SCRIPT = os.path.join(os.path.dirname(__file__), 'untar_gz_files.py')
+COMMIT_HASH = subprocess.check_output(['git', 'describe', '--always']).strip().decode()
+
+def get_path_components_from_path(path):
+    """
+    Returns the bucket_name and subdir for GS only paths
+    Uses regex to match the bucket name and the subdirectory.
+    """
+
+    path_components = (PATH_PATTERN.match(path)).groups()
+
+    bucket_name = path_components[0]
+    subdir = path_components[1]
+
+    return bucket_name, subdir
+
+def get_tarballs_from_path(bucket_name: str, subdir: str) -> list[str]:
+    """
+    Checks a gs://bucket/subdir/ path for .tar and .tar.gz files
+    Returns a list of:
+        - .tar and .tar.gz blob paths found in the subdirectory
+        - the size of image to use when unpacking the tarball
+    """
+    file_paths = [f.as_uri() for f in to_path(f'gs://{bucket_name}/{subdir}').iterdir()]
+    if not file_paths:
+        logging.info(f'No files found in {subdir} of {bucket_name}')
+        return []
+    
+    tarfiles = [f for f in file_paths if f.endswith(('.tar', '.tar.gz', '.tar.bz2'))]
+    logging.info(f'{len(tarfiles)} tar files found in gs://{bucket_name}/{subdir}')
+    
+    return tarfiles
+
+@click.command()
+@click.option(
+    '--search-path', '-p', help='GCP bucket/directory to search', required=True
+)
+def main(search_path: str):
+    """
+    Who runs the world? main()
+
+    Args:
+        search_path (str): path to find tarballs in
+    """
+    bucket_name, subdir = get_path_components_from_path(search_path)
+    blobs = get_tarballs_from_path(bucket_name, subdir)
+    if len(blobs) == 0:
+        logging.info('Nothing to do, quitting')
+        sys.exit(0)
+
+    # iterate over targets, set each one off in parallel
+    for blobname in blobs:
+        # create and config job
+        job = get_batch().new_job(name=f'Unzip {blobname}')
+        job.image(get_config()['workflow']['driver_image'])
+        job.cpu(4)
+        authenticate_cloud_credentials_in_job(job)
+        copy_common_env(job)
+        prepare_git_job(
+            job,
+            organisation='populationgenomics',
+            repo_name='analysis-runner',
+            commit=COMMIT_HASH,
+        )
+        job.command(f'')
+        job.cloudfuse(bucket_name, f'/{bucket_name}').command(
+            f"""
+            tar -xzf ~/{blobname.removeprefix('gs://')} -C ~/{bucket_name}/{subdir}/extracted/'
+            """
+        )
+
+    get_batch().run(wait=False)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(module)s:%(lineno)d - %(message)s',
+        datefmt='%Y-%M-%d %H:%M:%S',
+        stream=sys.stderr,
+    )
+
+    main()  # pylint: disable=no-value-for-parameter
