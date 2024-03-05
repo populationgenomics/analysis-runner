1af32972320b985b99d29bdeeb38daf410472804
EddieLF <eddieformaini@gmail.com>
2024-03-05 13:36:03 +1100

    Linting and documentation

--- scripts/generic_https_transfer.py
+++ scripts/generic_https_transfer.py
@@ -6,6 +6,7 @@ Transfer datasets from presigned URLs to a dataset's GCP main-upload bucket.
 
 import os
 from shlex import quote
+from typing import List
 
 import click
 from cloudpathlib import AnyPath
@@ -17,20 +18,38 @@ from cpg_utils.hail_batch import (
     get_batch,
 )
 import hailtop.batch.job as hb_job
-from typing import List
+
+
+def parse_presigned_url_file(
+    file_path: str, filenames: bool, garvan_manifest: bool
+) -> dict[str, str]:
+    """
+    Parse a presigned URL file to extract the filenames and presigned URLs
+    """
+    if garvan_manifest:
+        return parse_garvan_manifest(file_path)
+
+    with AnyPath(file_path).open() as file:
+        if filenames:
+            return {
+                line.strip().split(' ')[1]: line.strip().split(' ')[0]
+                for line in file.readlines()
+                if line.strip()
+            }
+        return {line.strip(): '' for line in file.readlines() if line.strip()}
 
 
 def parse_garvan_manifest(file_path: str):
     """
     Parse a Garvan manifest file to extract the filenames and presigned URLs
-    
+
     Example of a Garvan manifest file:
-    
+
     ```
     curl -Lo Sample01.tar.gz 'https://filesender.aarnet.edu.au/download.php?token=abc&files_ids=123456' # Expires 30 Mar 2024
     curl -Lo SummaryFiles.tar.gz 'https://filesender.aarnet.edu.au/download.php?token=xyz&files_ids=123457' # Expires 30 Mar 2024
     ```
-    
+
     Args:
         file_path (str): path to the Garvan manifest file
     Returns:
@@ -53,21 +72,49 @@ def parse_garvan_manifest(file_path: str):
     help='Use filenames defined before each url',
 )
 @click.option('--garvan-manifest', '-g', is_flag=True, help='File is a Garvan manifest')
-@click.option('--concurrent-job-cap', default=5, help='To limit the number of concurrent jobs, hopefully preventing cURL errors due to too many open connections')
 @click.option('--storage', default=100, help='Storage in GiB for each cURL job')
+@click.option(
+    '--concurrent-job-cap',
+    default=5,
+    help=(
+        'To limit the number of concurrent jobs, hopefully preventing cURL errors due to too many open connections'
+    ),
+)
 @click.option('--presigned-url-file-path')
-def main(presigned_url_file_path: str, filenames: bool, garvan_manifest: bool = False, concurrent_job_cap: int = 5, storage: int = 100):
+def main(
+    presigned_url_file_path: str,
+    filenames: bool,
+    garvan_manifest: bool = False,
+    storage: int = 100,
+    concurrent_job_cap: int = 5,
+):
     """
-    Given a list of presigned URLs, download the files and upload them to GCS.
-    If each signed url is prefixed by a filename and a space, use the --filenames flag
-    GCP suffix in target GCP bucket is defined using analysis-runner's --output
-    
-    If the file is a Garvan manifest, then parse the filenames and URLs from the manifest.
-    ex: download_links.txt, which contains two lines:
-    
-    
-    concurrent-job-cap is used to limit the number of concurrent jobs. Too many concurrent jobs can cause cURL errors due to too many open connections.
-    ex. curl: (55) OpenSSL SSL_write: Connection reset by peer, errno 104
+    Given a list of presigned URLs, download the files to disk and then upload them to GCS.
+
+    --presigned-url-file-path is a file with a list of presigned URLs, one per line.
+
+    **Formats:**
+      - Just URLs, no filenames (default):
+        ```
+        https://example.com/file1
+        https://example.com/file2
+        ```
+      - With filenames (use --filenames flag):
+        ```
+        file1 https://example.com/file1
+        file2 https://example.com/file2
+        ```
+      - Garvan manifest (use --garvan-manifest / -g flag):
+        ```
+        curl -Lo Sample01.tar.gz 'https://filesender.aarnet.edu.au/download.php?token=abc&files_ids=123456'
+        curl -Lo SummaryFiles.tar.gz 'https://filesender.aarnet.edu.au/download.php?token=xyz&files_ids=123457'
+        ```
+
+    --output-dir (from analysis-runner submission) defines the output file prefix in target GCP bucket.
+    --storage is the amount of storage in GiB for each cURL job.
+    --concurrent-job-cap is used to limit the number of concurrent jobs.
+      - Too many concurrent jobs can cause cURL errors due to too many open connections.
+        - e.g. curl: (55) OpenSSL SSL_write: Connection reset by peer, errno 104
     """
 
     env_config = get_config()
@@ -76,23 +123,18 @@ def main(presigned_url_file_path: str, filenames: bool, garvan_manifest: bool = 
     dataset = env_config['workflow']['dataset']
     output_prefix = env_config['workflow']['output_prefix']
     assert all({billing_project, cpg_driver_image, dataset, output_prefix})
-    
-    if garvan_manifest:
-        urls_filenames = parse_garvan_manifest(presigned_url_file_path)
-    else:        
-        with AnyPath(presigned_url_file_path).open() as file:
-            if filenames:
-                urls_filenames = {line.strip().split(' ')[1]: line.strip().split(' ')[0] for line in file.readlines() if line.strip()}
-            else:
-                urls_filenames = {line.strip(): '' for line in file.readlines() if line.strip()}
+
+    urls_filenames = parse_presigned_url_file(
+        presigned_url_file_path, filenames, garvan_manifest
+    )
 
     incorrect_urls = [url for url in urls_filenames if not url.startswith('https://')]
     if incorrect_urls:
         raise ValueError(f'Incorrect URLs: {incorrect_urls}')
-    
+
     batch = get_batch(name=f'transfer {dataset}', default_image=cpg_driver_image)
     all_jobs: List[hb_job.Job] = []
-    
+
     def manage_concurrency(new_job: hb_job.Job):
         """
         Manage concurrency, so that there is a cap on simultaneous jobs
@@ -104,17 +146,28 @@ def main(presigned_url_file_path: str, filenames: bool, garvan_manifest: bool = 
         all_jobs.append(new_job)
 
     output_path = dataset_path(output_prefix, 'upload')
+
+    # Check if any files already exist in the output path
     files_in_output = [f.as_uri() for f in to_path(output_path).iterdir()]
     if files_in_output:
         print(f'Files found in {output_path}:')
         print(files_in_output)
 
-    # may as well batch them to reduce the number of VMs
-    for url, filename in urls_filenames.items():
+    used_filenames = []
+    for i, (url, filename) in enumerate(urls_filenames.items()):
         if filenames or garvan_manifest:
             if to_path(os.path.join(output_path, filename)).as_uri() in files_in_output:
                 print(f'File {filename} already exists in {output_path}')
-                continue 
+                continue
+
+        if not filename:
+            filename = os.path.basename(url).split('?')[0]
+            if filename in used_filenames:
+                # Add a number to avoid overwriting files
+                filename = f'{i}_{filename}'
+            used_filenames.append(filename)
+
+        # Create a new job for each file
         j = batch.new_job(f'cURL ({filename})')
         j.storage(f'{storage}Gi')
         quoted_url = quote(url)
@@ -124,7 +177,7 @@ def main(presigned_url_file_path: str, filenames: bool, garvan_manifest: bool = 
         j.command(f'curl -C - -Lf {quoted_url} -o {filename}')
         j.command(f'gsutil -m cp {filename} {output_path}')
         manage_concurrency(j)
-            
+
     batch.run(wait=False)
 
 
