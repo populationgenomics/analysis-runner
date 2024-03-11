8edcfd09e801af1a9443dd1e545f8304ea517d58
EddieLF <eddieformaini@gmail.com>
2024-03-11 16:29:07 +1100

    Linting

--- scripts/untar_gz_files.py
+++ scripts/untar_gz_files.py
@@ -79,12 +79,12 @@ def main(bucket: str, subdir: str, blob_name: str, outdir: str):
     # Move the tarball to the "completed" directory
     subprocess.run(
         [
-            'gsutil', 
-            'mv', 
-            f'gs://{bucket}/{subdir}/{os.path.basename(blob_name)}', 
-            f'gs://{bucket}/{subdir}/completed/{os.path.basename(blob_name)}'
-        ], 
-        check=True
+            'gsutil',
+            'mv',
+            f'gs://{bucket}/{subdir}/{os.path.basename(blob_name)}',
+            f'gs://{bucket}/{subdir}/completed/{os.path.basename(blob_name)}',
+        ],
+        check=True,
     )
     logging.info('All tarballs extracted and uploaded. Finishing...')
 
