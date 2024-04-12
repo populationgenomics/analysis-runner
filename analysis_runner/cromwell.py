78c25ceb55007d762043d4c1124d20f69e61fb34
Michael Franklin <illusional@users.noreply.github.com>
2024-04-12 15:43:30 +1000

    Re-add deprecated cromwell stub

---
+++ analysis_runner/cromwell.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+
+from deprecated import deprecated
+
+# old import that's deprecated, use the cpg_utils one instead
+from cpg_utils.cromwell import CromwellOutputType
+
+
+@deprecated('Use cpg_utils.cromwell.run_cromwell_workflow_from_repo_and_get_outputs')
+def run_cromwell_workflow_from_repo_and_get_outputs(*args, **kwargs):
+    from cpg_utils.cromwell import run_cromwell_workflow_from_repo_and_get_outputs as f
+
+    return f(*args, **kwargs)
