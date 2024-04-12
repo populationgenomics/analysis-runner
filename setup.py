9a0820a3c8de0064a2dab1210cf468836d427a0a
Michael Franklin <illusional@users.noreply.github.com>
2024-04-12 15:43:48 +1000

    Pin cpg-utils to >= 5.0.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     zip_safe=False,
     install_requires=[
         'cloudpathlib[all]',
-        'cpg-utils>=4.12.1',
+        'cpg-utils>=5.0.0',
         'hail',
         'requests',
         'tabulate',
