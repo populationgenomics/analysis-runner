d957d926ca9fcaa5317727b1c0ba711c5734e7ac
Michael Franklin <illusional@users.noreply.github.com>
2024-05-15 19:26:56 +1000

    Fix test to run

--- examples/cromwell/run-gcp-batch-test.sh
+++ examples/cromwell/run-gcp-batch-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pip3 install --no-deps --force git+https://github.com/populationgenomics/cpg-utils.git@update-workflow-options-for-cromwell-gcs-batch
 
-python3 run-gcp-batch-test.sh
+python3 cromwell_from_hail_batch.py
