cba832e19fb5a8645aac2f74239b7466be40efb9
Michael Franklin <illusional@users.noreply.github.com>
2024-05-15 19:22:23 +1000

    Add shebang to be lazy

--- examples/cromwell/run-gcp-batch-test.sh
+++ examples/cromwell/run-gcp-batch-test.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 pip3 install --no-deps --force git+https://github.com/populationgenomics/cpg-utils.git@update-workflow-options-for-cromwell-gcs-batch
 
 python3 run-gcp-batch-test.sh
