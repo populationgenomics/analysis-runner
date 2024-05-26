34b5a2727016ea2550e6a8ab87c1995e7037ccfd
Michael Franklin <illusional@users.noreply.github.com>
2024-05-27 08:04:20 +1000

    Manually set metadata IP server

--- scripts/hail_gcp_auth_test.sh
+++ scripts/hail_gcp_auth_test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+set +x
 gcloud auth list
 
 echo $GCE_METADATA_IP
 echo $GCE_METADATA_ROOT
 
+export GCE_METADATA_IP=169.254.169.254
+export GCE_METADATA_ROOT=169.254.169.254
+
+gsutil ls -lh gs://cpg-fewgenomes-test/
+
 curl \
     -H "Metadata-Flavor: Google" \
-    "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts"
+    "http://$GCE_METADATA_ROOT/computeMetadata/v1/instance/service-accounts"
