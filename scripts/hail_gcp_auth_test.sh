b4d470c56582e9ac79d65c39858e40f551bf9dd7
Michael Franklin <illusional@users.noreply.github.com>
2024-05-27 08:06:06 +1000

    Print all commands

--- scripts/hail_gcp_auth_test.sh
+++ scripts/hail_gcp_auth_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set +x
+set -x
 gcloud auth list
 
 echo $GCE_METADATA_IP
 echo $GCE_METADATA_ROOT
 
-export GCE_METADATA_IP=169.254.169.254
-export GCE_METADATA_ROOT=169.254.169.254
+# export GCE_METADATA_IP=169.254.169.254
+# export GCE_METADATA_ROOT=169.254.169.254
 
 gsutil ls -lh gs://cpg-fewgenomes-test/
 
