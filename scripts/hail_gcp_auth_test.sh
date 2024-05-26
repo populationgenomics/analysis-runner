c060574be590ec44556cd520696d3ecf1ff519c7
Michael Franklin <illusional@users.noreply.github.com>
2024-05-27 09:59:22 +1000

    Try gcloud storage

--- scripts/hail_gcp_auth_test.sh
+++ scripts/hail_gcp_auth_test.sh
@@ -2,18 +2,11 @@
 set -x
 gcloud auth list
 
-echo $GCE_METADATA_IP
-echo $GCE_METADATA_ROOT
+# echo $GCE_METADATA_IP
+# echo $GCE_METADATA_ROOT
 
 # export GCE_METADATA_IP=169.254.169.254
 # export GCE_METADATA_ROOT=169.254.169.254
 
-gsutil ls -lh gs://cpg-fewgenomes-test/
+gcloud storage ls -lh gs://cpg-fewgenomes-test/
 
-curl \
-    -H "Metadata-Flavor: Google" \
-    "http://$GCE_METADATA_ROOT/computeMetadata/v1/instance/service-accounts"
-
-curl \
-    -H "Metadata-Flavor: Google" \
-    "http://$GCE_METADATA_ROOT/computeMetadata/v1/instance/service-accounts/default/token"
