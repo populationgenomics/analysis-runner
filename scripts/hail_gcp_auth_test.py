010337b4e43e9b203abf893f51c2deb5f5642c9a
Michael Franklin <illusional@users.noreply.github.com>
2024-05-27 11:35:16 +1000

    Add test workflow without checkout or other activation

---
+++ scripts/hail_gcp_auth_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from cpg_utils.config import get_driver_image
+from cpg_utils.hail_batch import get_batch
+
+b = get_batch('Hail GCP auth test')
+
+j = b.new_bash_job('Three ls approaches')
+
+j.image(get_driver_image())
+j.command(
+    """
+    set -x
+    gsutil ls -lh gs://cpg-fewgenomes-test/
+    gcloud storage ls gs://cpg-fewgenomes-test/
+
+    python3 -c '
+    from google.cloud import storage;
+    client = storage.Client();
+    bucket = client.get_bucket("cpg-fewgenomes-test");
+    print([a.name for a in bucket.list_blobs(prefix="", delimiter="/")])
+    '
+    """,
+)
+
+b.run(wait=False)
