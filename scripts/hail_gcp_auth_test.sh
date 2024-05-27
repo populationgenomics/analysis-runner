77805835f816308d7204b946bceac79b2091478c
Michael Franklin <illusional@users.noreply.github.com>
2024-05-27 11:29:29 +1000

    Re-add gsutil ls + other mechanisms

--- scripts/hail_gcp_auth_test.sh
+++ scripts/hail_gcp_auth_test.sh
@@ -8,5 +8,12 @@ gcloud auth list
 # export GCE_METADATA_IP=169.254.169.254
 # export GCE_METADATA_ROOT=169.254.169.254
 
+gutil ls -lh gs://cpg-fewgenomes-test/
 gcloud storage ls gs://cpg-fewgenomes-test/
 
+python3 -c '
+    from google.cloud import storage;
+    client = storage.Client();
+    bucket = client.get_bucket("cpg-fewgenomes-test");
+    print([a.name for a in bucket.list_blobs(prefix="", delimiter="/")])
+'
