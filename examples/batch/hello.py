339f170d6643b0c5d6d2dd8eec724c0a130edfea
Joshua Schmidt <joshmschmidt1@gmail.com>
2025-02-26 14:59:41 +1100

    jossch changes

--- examples/batch/hello.py
+++ examples/batch/hello.py
@@ -9,8 +9,10 @@ Test Hail Batch Workflow
       --description "Run Batch" \
       --output-dir "$(whoami)/hello-world" \
       hello.py \
-      --name-to-print $(whoami)
+      --name-of-sender $(whoami) \
+      --name-of-receiver ${string}
 """
+
 from shlex import quote
 
 import click
@@ -19,28 +21,31 @@ from cpg_utils.hail_batch import get_batch
 
 
 @click.command()
-@click.option('--name-to-print')
+@click.option("--name-of-sender")
+@click.option("--name-of-receiver")
 def main(name_to_print: str):
     """Runs test hail batch workflow"""
 
     b = get_batch()
 
-    j1 = b.new_job('first job')
+    j1 = b.new_job(f"first job from, {name_of_sender}")
     # For Hail batch, j.{identifier} will create a Resource (file)
     # that will be collected at the end of a job
     stdout_of_j = j1.out
-    string_to_print = f'Hello, {name_to_print}'
-    j1.command(f'echo {quote(string_to_print)} > {stdout_of_j}')
+    string_sender = f"Hello, {name_of_receiver}, it's me, {name_of_sender}"
+    string_receiver = f"Hello, {name_of_sender}, thanks for the shout out!"
+    j1.command(f"echo {quote(string_sender)} > {stdout_of_j}")
+    j1.command(f"echo {quote(string_receiver)} > {stdout_of_j}")
 
-    j2 = b.new_job('second job')
+    j2 = b.new_job("second job")
     # for the second job, using an f-string with the resource file
     # will tell batch to run j2 AFTER j1
-    j2.command(f'cat {stdout_of_j}')
+    j2.command(f"cat {stdout_of_j}")
 
     # use wait=False, otherwise this line will hang while the sub-batch runs
     # bad for running hail batch within a hail batch, as preemption
     b.run(wait=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
