baf613ed0bfc64b03dc9e8b777e20ec2ecbfedf1
Dan Coates <dan.coates@populationgenomics.org.au>
2025-03-26 13:30:31 +1100

    cache expensive network calls to improve performance
    The webserver request handler had a couple of calls, one to load the
    server config secret, and one to get the membership cache that were
    relatively expensive, taking between 1 and 2 seconds each. This update
    caches those calls for a short period of time so that web pages that
    make many requests for small files will not have to make these expensive
    network calls on each request.

--- web/main.py
+++ web/main.py
@@ -34,12 +34,52 @@ storage_client = google.cloud.storage.Client()
 logger = logging.getLogger('gunicorn.error')
 
 
+# These calls for server config and permissions are quite expensive
+# so cache them for a little while to avoid long load times for pages
+# with lots of subsequent requests
+@cached(cache=TTLCache(maxsize=64, ttl=60))
+def get_server_config(project_id: str):
+    server_config_raw = read_secret(project_id, 'server-config')
+    if not server_config_raw:
+        raise Exception('failed to read server-config secret')
+    return json.loads(server_config_raw)
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=60))
+def has_permission(email: str, dataset: str, access_file: str | None, bucket_name: str):
+    bucket = storage_client.bucket(bucket_name)
+
+    if is_member_in_cached_group(
+        f'{dataset}-web-access',
+        email,
+        members_cache_location=MEMBERS_CACHE_LOCATION,
+    ):
+        return True
+
+    # Second chance: if there's a '.access' file in the first subdirectory,
+    # check if the email is listed there.
+    if access_file:
+        blob = bucket.get_blob(access_file)
+        if blob is None:
+            return False
+        access_list = blob.download_as_text().lower().splitlines()
+        if email in access_list:
+            return True
+
+        logger.warning(
+            f'{email} is not in {dataset} access group or {access_file}',
+        )
+        return False
+    return False
+
+
 @app.route('/<dataset>/<path:filename>')
-def handler(  # noqa: C901
+def handler(
     dataset: Optional[str] = None,
     filename: Optional[str] = None,
 ):
     """Main entry point for serving."""
+
     if not dataset or not filename:
         logger.warning('Invalid request parameters')
         return abort(400, 'Either the dataset or filename was not present')
@@ -67,11 +107,12 @@ def handler(  # noqa: C901
     if os.path.basename(filename) == '.access':
         return abort(403, 'Unable to read .access files')
 
-    server_config_raw = read_secret(ANALYSIS_RUNNER_PROJECT_ID, 'server-config')
-    if not server_config_raw:
-        logger.exception('Failed to read server-config secret')
+    try:
+        server_config = get_server_config(ANALYSIS_RUNNER_PROJECT_ID)
+    except Exception as e:
+        logger.exception(f'Failed to read server-config secret: {e}')
         return abort(500, 'Failed to read server-config secret')
-    server_config = json.loads(server_config_raw)
+
     if dataset not in server_config:
         logger.warning(f'Invalid dataset "{dataset}"')
         return abort(403, 'Invalid dataset')
@@ -79,32 +120,16 @@ def handler(  # noqa: C901
     bucket_name = f'cpg-{dataset}-{BUCKET_SUFFIX}'
     bucket = storage_client.bucket(bucket_name)
 
-    in_web_access_group = False
-    try:
-        in_web_access_group = is_member_in_cached_group(
-            f'{dataset}-web-access',
-            email,
-            members_cache_location=MEMBERS_CACHE_LOCATION,
-        )
-    except Exception as e:  # noqa: BLE001
-        logger.warning(f'Failed to access group membership cache: {e}')
+    access_file: str | None = None
+    split_subdir = filename.split('/', maxsplit=1)
+    if len(split_subdir) == 2 and split_subdir[0]:  # noqa: PLR2004
+        access_file = f'{split_subdir[0]}/.access'
 
-    if not in_web_access_group:
-        # Second chance: if there's a '.access' file in the first subdirectory,
-        # check if the email is listed there.
-        split_subdir = filename.split('/', maxsplit=1)
-        if len(split_subdir) == 2 and split_subdir[0]:  # noqa: PLR2004
-            access_list_filename = f'{split_subdir[0]}/.access'
-            blob = bucket.get_blob(access_list_filename)
-            if blob is None:
-                logger.warning(f'{email} is not a member of the {dataset} access group')
-                return abort(403)
-            access_list = blob.download_as_text().lower().splitlines()
-            if email not in access_list:
-                logger.warning(
-                    f'{email} is not in {dataset} access group or {access_list_filename}',
-                )
-                return abort(403)
+    if not has_permission(email, dataset, access_file, bucket_name):
+        logger.warning(
+            f'{email} is not in {dataset} access group or access list',
+        )
+        return abort(403)
 
     logger.info(f'Fetching blob gs://{bucket_name}/{filename}')
     blob = bucket.get_blob(filename)
