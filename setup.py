0ee174158a03941cffbba7624005ce47c1d95d4e
Dan Coates <dan.coates@populationgenomics.org.au>
2025-03-26 13:04:23 +1100

    specify version of hail as an old version (0.2.130) was being installed

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setuptools.setup(
         # old version of protobuf and recent versions of grpcio-status requiring
         # a much newer version
         'grpcio-status>=1.48,<1.50',
-        'orjson~=3.9.15',  # pip wants to install 3.9.10 but that hits vuln PYSEC-2024-40 so need to explicitly list this
-        'hail',
+        'hail>=0.2.134',
         'requests',
         'tabulate',
     ],
