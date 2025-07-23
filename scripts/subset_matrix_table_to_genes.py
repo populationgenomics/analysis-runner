2f1ef7967e2fb746cdff0cbf5eb058886654dbbb
Matt Welland <mattwellie@gmail.com>
2025-07-23 15:18:29 +1000

    revert to parse args (#737)

--- scripts/subset_matrix_table_to_genes.py
+++ scripts/subset_matrix_table_to_genes.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
         default='mt',
         choices=['both', 'mt', 'vcf', 'ht'],
     )
-    args, unknown = parser.parse_args()
+    args = parser.parse_args()
 
     init_batch()
 
