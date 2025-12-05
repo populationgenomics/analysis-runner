0fdaf6317d8dbf3d6d12a7061ece7fe350089a2c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-12-05 13:02:34 +1100

    Update meta_analysis_zip.sh

--- meta_analysis_zip.sh
+++ meta_analysis_zip.sh
@@ -2,26 +2,25 @@
 set -euo pipefail
 
 BASE_PREFIX="gs://cpg-tenk10k-test-analysis/str/associatr/final_freeze/tob_n950_and_bioheart_n975/trs_snps/zips"
-OUTPUT_GCS="gs://cpg-tenk10k-test-analysis/str/associatr/final_freeze/tob_n950_and_bioheart_n975/trs_snps/master_zip/meta_NK_Proliferating_Plasmablast_Treg_cDC1_cDC2.zip"
+OUTPUT_GCS="gs://cpg-tenk10k-test-analysis/str/associatr/final_freeze/tob_n950_and_bioheart_n975/trs_snps/master_zip/meta_dnT_gdT_pDC.zip"
 
 # Work in a temp dir
 TMPDIR=$(mktemp -d)
 cd "$TMPDIR"
 
 echo "Copying all cell-type zips from $BASE_PREFIX ..."
-gsutil -m cp "${BASE_PREFIX}/NK_Proliferating.zip" . 
-gsutil -m cp "${BASE_PREFIX}/Plasmablast.zip" . 
-gsutil -m cp "${BASE_PREFIX}/Treg.zip" . 
-gsutil -m cp "${BASE_PREFIX}/cDC1.zip" . 
-gsutil -m cp "${BASE_PREFIX}/cDC2.zip" . 
+gsutil -m cp "${BASE_PREFIX}/dnT.zip" . 
+gsutil -m cp "${BASE_PREFIX}/gdT.zip" . 
+gsutil -m cp "${BASE_PREFIX}/pDC.zip" . 
+
 
 
 echo "Creating meta_analysis.zip from:"
 ls *.zip
 
-zip -r meta_NK_Proliferating_Plasmablast_Treg_cDC1_cDC2.zip *.zip
+zip -r meta_dnT_gdT_pDC.zip *.zip
 
 echo "Uploading meta_analysis.zip to $OUTPUT_GCS ..."
-gsutil cp meta_NK_Proliferating_Plasmablast_Treg_cDC1_cDC2.zip "$OUTPUT_GCS"
+gsutil cp meta_dnT_gdT_pDC.zip "$OUTPUT_GCS"
 
 echo "Done."
