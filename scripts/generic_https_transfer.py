450fecd84ab94be93cd618712f7ad5dec32461f7
Amy Miniter <40307120+amyminiter@users.noreply.github.com>
2026-01-23 08:28:35 +1100

    Understand comment and update quote() properly

--- scripts/generic_https_transfer.py
+++ scripts/generic_https_transfer.py
@@ -76,18 +76,17 @@ def main(presigned_url_file_path: str, filenames: bool, use_wget: bool):
             j.spot(is_spot=False)
 
         quoted_source_url = quote(url)
-        quoted_output_path = quote(output_path)
         authenticate_cloud_credentials_in_job(job=j)
         # catch errors during the cURL
         j.command('set -euxo pipefail')
 
         if use_wget:
             j.command(
-                f'wget -O - {quoted_source_url} | gsutil cp - {os.path.join(quoted_output_path, filename)}',
+                f'wget -O - {quoted_source_url} | gsutil cp - {quote(os.path.join(output_path, filename))}',
             )
         else:
             j.command(
-                f'curl -L {quoted_source_url} | gsutil cp - {os.path.join(quoted_output_path, filename)}',
+                f'curl -L {quoted_source_url} | gsutil cp - {quote(os.path.join(output_path, filename))}',
             )
 
     batch.run(wait=False)
