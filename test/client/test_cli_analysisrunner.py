5e2a9c8b564757ba4e5f4703df2c29f897fb0a54
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2026-07-06 13:45:02 +1200

    Three more tests for repo checkout

--- test/client/test_cli_analysisrunner.py
+++ test/client/test_cli_analysisrunner.py
@@ -12,6 +12,8 @@ class AnalysisRunnerArgs(TypedDict):
     access_level: str
     description: str
     output_dir: str
+    skip_repo_checkout: bool
+    repository: str | None
     script: list[str]
 
 
@@ -20,6 +22,8 @@ ANALYSIS_RUNNER_ARGS: AnalysisRunnerArgs = {
     'access_level': 'test',
     'description': 'mock-test',
     'output_dir': 'hello-world-test',
+    'skip_repo_checkout': True,
+    'repository': None,
     'script': [],
 }
 
@@ -74,3 +78,43 @@ def test_run_analysis_runner_accept_full_access(
     assert posted_requests['json']['accessLevel'] == 'full'
     assert posted_requests['headers']['Authorization'] == 'Bearer fake-token'
     assert 'Request submitted successfully: ok' in caplog.text
+
+
+def test_skip_repository_checkout(mocker, posted_requests, caplog) -> None:  # noqa: ANN001 ARG001
+    caplog.set_level(logging.INFO, logger='analysis_runner')
+    args: AnalysisRunnerArgs = {
+        **ANALYSIS_RUNNER_ARGS,
+        'script': ['echo hello world'],
+    }
+    mock_get_repository_specific_information = mocker.patch(
+        'analysis_runner.cli_analysisrunner.get_repository_specific_information',
+    )
+    run_analysis_runner(**args)
+    mock_get_repository_specific_information.assert_not_called()
+    assert f'Submitting analysis for dataset "{args["dataset"]}"\n' in caplog.text
+
+
+def test_supply_repository_but_not_commit() -> None:
+    args: AnalysisRunnerArgs = {
+        **ANALYSIS_RUNNER_ARGS,
+        'skip_repo_checkout': False,
+        'repository': 'https://github.com/populationgenomics/analysis-runner',
+        'script': ['echo hello world'],
+    }
+    with pytest.raises(
+        ValueError, match="You must supply the '--commit <SHA>' parameter "
+    ):
+        run_analysis_runner(**args)
+
+
+def test_dont_skip_repository_checkout(posted_requests, mocker) -> None:  # noqa: ANN001 ARG001
+    args: AnalysisRunnerArgs = {
+        **ANALYSIS_RUNNER_ARGS,
+        'skip_repo_checkout': False,
+        'script': ['echo hello world'],
+    }
+    mock_get_repository_specific_information = mocker.patch(
+        'analysis_runner.cli_analysisrunner.get_repository_specific_information',
+    )
+    run_analysis_runner(**args)
+    mock_get_repository_specific_information.assert_called_once()
