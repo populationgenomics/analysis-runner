1a673d3d1c47d11c86ecf608cb77b89e8381032c
hrrsheen <harrison.sheen@populationgenomics.org.au>
2026-07-29 14:41:25 +1000

    Switched to using metamist package

--- metamist/main.py
+++ metamist/main.py
@@ -9,10 +9,11 @@ import json
 import logging
 import os
 from typing import Any, Literal
-from urllib.parse import urlencode
 
 import google.cloud.logging
-import requests
+from metamist.api_client import ApiClient
+from metamist.apis import AnalysisRunnerApi
+from metamist.configuration import Configuration
 
 # 1. Initialize the Cloud Logging client
 client = google.cloud.logging.Client()
@@ -39,70 +40,37 @@ def main(data: dict[Literal['data'], str], unused_context: Any):
         or DEFAULT_AUDIENCE_URL
     )
 
-    # remove them from the metadata object so we can pass the remaining values as meta
-    project = metadata.pop('dataset')
-    access_level = metadata.get('accessLevel')
-    ar_guid = metadata.pop('ar-guid')
-    user = metadata.pop('user')
-    access_level = metadata.pop('accessLevel')
-    repo = metadata.pop('repo')
-    commit = metadata.pop('commit')
-    script = metadata.pop('script')
-    description = metadata.pop('description')
-    output_prefix = metadata.pop('output')
-    driver_image = metadata.pop('driverImage')
-    config_path = metadata.pop('configPath')
-    cwd = metadata.pop('cwd')
-    environment = metadata.pop('environment')
-    hail_version = metadata.pop('hailVersion', None)
-    batch_url = metadata.pop('batch_url', None)
-    meta = metadata.pop('meta', {}) or {}
-
+    project = metadata['dataset']
+    access_level = metadata['accessLevel']
     if access_level == 'test':
         project += '-test'
 
-    query_params = {
-        'ar_guid': ar_guid,
+    kwargs = {
+        'project': project,
+        'ar_guid': metadata['ar-guid'],
         'access_level': access_level,
-        'repository': repo,
-        'commit': commit,
-        'script': script,
-        'description': description,
-        'driver_image': driver_image,
-        'config_path': config_path,
-        'cwd': cwd,
-        'environment': environment,
-        'hail_version': hail_version,
-        'batch_url': batch_url,
-        'submitting_user': user,
-        'output_path': output_prefix,
+        'repository': metadata['repo'],
+        'commit': metadata['commit'],
+        'script': metadata['script'],
+        'description': metadata['description'],
+        'driver_image': metadata['driverImage'],
+        'config_path': metadata['configPath'],
+        'cwd': metadata['cwd'],
+        'environment': metadata['environment'],
+        'hail_version': metadata.get('hailVersion'),
+        'batch_url': metadata.get('batch_url') or '',
+        'submitting_user': metadata['user'],
+        'output_path': metadata['output'],
+        'request_body': metadata.get('meta') or {},
     }
-    q = urlencode(query_params)
+
+    api_client = ApiClient(Configuration(host=audience))
+    ar_api = AnalysisRunnerApi(api_client)
 
     try:
-        token = get_identity_token(audience)
-        r = requests.put(
-            f'{audience}/api/v1/analysis-runner/{project}/?' + q,
-            json=meta,
-            headers={'Authorization': f'Bearer {token}'},
-            timeout=60,
-        )
-        r.raise_for_status()
-        analysis_id = r.text
+        analysis_id = ar_api.create_analysis_runner_log(**kwargs)
         logging.info(f'Created analysis with ID = {analysis_id}')
         return analysis_id
-    except requests.exceptions.HTTPError as err:
-        logging.exception(f'Failed with response: {err.response.text}')
-        raise err
-
-
-def get_identity_token(audience: str) -> str:
-    """
-    Get identity token
-    Source: https://cloud.google.com/functions/docs/securing/function-identity#identity_tokens
-    """
-    meta_url = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity'
-    url = f'{meta_url}?audience={audience}&format=full'
-    r = requests.get(url=url, headers={'Metadata-Flavor': 'Google'}, timeout=30)
-    r.raise_for_status()
-    return r.text
+    except Exception:
+        logging.exception('Failed to create analysis-runner log')
+        raise
