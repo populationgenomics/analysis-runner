1e7473ef705af2628e1271a62bc4c8262a0f2650
Dan Coates <dan.coates@populationgenomics.org.au>
2026-09-18 16:38:42 +1000

    Add CLI for launching nextflow jobs via seqera platform
    This generally aligns more closely with seqera cli's `tw launch` than
    with existing analysis-runner conventions.
    https://docs.seqera.io/platform-cli/reference/launch
    
    Though there is some analysis-runner logic applied - or at least
    scaffolded to be applied in the server side implementation.
    
    - Rather than specifying compute env and workspace, you specify dataset
      and access level, and the server will look up the compute env and
      workspace from those.
    - Overriding of config with local config is disallowed for access levels
      other than test. This is because some config fields could be used to
      execute arbitrary code on the workflow runner. For access levels
      standard/full, a config can be specified but it must abide by the same
      rules as the workflow itself, ie. being on the main branch of an
      allow-listed repository.
    - Several other `tw launch` args are omitted as we don't have a clear
      use case yet, they can be added later if needed.

--- packages/analysis-runner/src/analysis_runner/cli.py
+++ packages/analysis-runner/src/analysis_runner/cli.py
@@ -16,6 +16,7 @@ from analysis_runner.cli_analysisrunner import (
 )
 from analysis_runner.cli_config import add_config_args, run_config_from_args
 from analysis_runner.cli_cromwell import add_cromwell_args, run_cromwell_from_args
+from analysis_runner.cli_seqera import add_seqera_args, run_seqera_from_args
 
 
 def main_from_args(args: Sequence[str] | None = None):
@@ -37,6 +38,7 @@ def main_from_args(args: Sequence[str] | None = None):
     modes: dict[str, tuple[Callable[[], argparse.ArgumentParser], Callable]] = {
         'analysis-runner': (add_analysis_runner_args, run_analysis_runner_from_args),
         'cromwell': (add_cromwell_args, run_cromwell_from_args),
+        'seqera': (add_seqera_args, run_seqera_from_args),
         'config': (add_config_args, run_config_from_args),
     }
 
